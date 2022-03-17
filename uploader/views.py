b382fa815774183be9de9a30160de2026f987271
alexsoft037 <alexsoft037@outlook.com>
2022-03-17 09:04:19 -0700

    first commit

---
+++ uploader/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from .models import File
+
+def index(request):
+    if request.method == 'POST':  
+        file = request.FILES['file'].read()
+        fileName= request.POST['filename']
+        existingPath = request.POST['existingPath']
+        end = request.POST['end']
+        nextSlice = request.POST['nextSlice']
+        
+        if file=="" or fileName=="" or existingPath=="" or end=="" or nextSlice=="":
+            res = JsonResponse({'data':'Invalid Request'})
+            return res
+        else:
+            if existingPath == 'null':
+                path = 'static/upload/' + fileName
+                with open(path, 'wb+') as destination: 
+                    destination.write(file)
+                FileFolder = File()
+                FileFolder.existingPath = fileName
+                FileFolder.eof = end
+                FileFolder.name = fileName
+                FileFolder.save()
+                if int(end):
+                    res = JsonResponse({'data':'Uploaded Successfully','existingPath': fileName})
+                else:
+                    res = JsonResponse({'existingPath': fileName})
+                return res
+    return render(request, 'upload.html')
